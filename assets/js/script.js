f0989dcc08a905a1710d398c5441e236a5afc4e9
Sheyla <Sheylapopovich@gmail.com>
2022-02-19 18:13:53 -0700

    inserted 0-5 switch cases for 8ball responses

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
 console.log("This is Linked!");
 
 let userName = "";
-let userQuestion = "when will I get married"
+let userQuestion = "when will I get married";
 const randomNumber = Math.floor(Math.random() * 8);
-const eightBall = ""
+const eightBall = "";
 
+console.log(`${userName} is asking `);
 
-console.log(`${userName} is asking `)
+switch (condition) {
+  case 0:
+    eightBall = "It is certain.";
+    break;
+  case 1:
+    eightBall = "It is decidedly so.";
+    break;
+  case 2:
+    eightBall = "Without a doubt.";
+    break;
+  case 3:
+    eightBall = "Yes definitely.";
+    break;
+  case 4:
+    eightBall = "You may rely on it.";
+    break;
+  case 5:
+    eightBall = "As I see it, yes.";
+    break;
+}
