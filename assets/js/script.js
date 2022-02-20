55a93e5fc094ea53700cda81a8a4316ec06808b8
Sheyla <Sheylapopovich@gmail.com>
2022-02-19 17:53:08 -0700

    created randomNumber variable to generate random number

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,7 @@ console.log("This is Linked!");
 
 let userName = "";
 let userQuestion = "when will I get married"
+const randomNumber = Math.floor(Math.random() * 8);
+
 
 console.log(`${userName} is asking `)
