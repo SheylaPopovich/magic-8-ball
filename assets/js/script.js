c0e29073d9d4321bb800c63d9ede89ff5ec6a18b
Sheyla <Sheylapopovich@gmail.com>
2022-02-19 18:07:06 -0700

    created eightBall variable to work as eightball

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ console.log("This is Linked!");
 let userName = "";
 let userQuestion = "when will I get married"
 const randomNumber = Math.floor(Math.random() * 8);
+const eightBall = ""
 
 
 console.log(`${userName} is asking `)
